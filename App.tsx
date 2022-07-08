e3002f6fbd82510cecf32c90eefa83032a2e23cc
yoshimura <karateyosi@gmail.com>
2022-07-08 18:26:12 +0900

    Add: hooks

--- App.tsx
+++ App.tsx
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { ItemType, useFlexDirectionBasics } from "./hooks";
 
 export default function App() {
+  return <FlexDirectionBasics />;
+}
+
+const FlexDirectionBasics = () => {
+  const { items } = useFlexDirectionBasics();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView style={{ marginTop: 40, marginBottom: 20 }}>
+      {items.map((item) => (
+        <PreviewLayout
+          label={item.label}
+          values={item.values}
+          selectedValue={item.selectedValue}
+          setSelectedValue={item.setSelectedValue}
+        >
+          <View style={[styles.box, { backgroundColor: "powderblue" }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View
+            style={[
+              styles.box,
+              { backgroundColor: "steelblue" },
+              item.extendStyle,
+            ]}
+          />
+        </PreviewLayout>
+      ))}
+    </ScrollView>
+  );
+};
+
+type PreviewLayoutType = {
+  children: JSX.Element[];
+} & ItemType;
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}: PreviewLayoutType) => {
+  return (
+    <View style={{ padding: 10, flex: 1 }}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.row}>
+        {values.map((value) => (
+          <TouchableOpacity
+            key={value}
+            onPress={() => setSelectedValue(value)}
+            style={[styles.button, selectedValue === value && styles.selected]}
+          >
+            <Text
+              style={[
+                styles.buttonLabel,
+                selectedValue === value && styles.selectedLabel,
+              ]}
+            >
+              {value}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={[styles.container, { [label]: selectedValue }]}>
+        {children}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 8,
+    backgroundColor: "aliceblue",
+    height: 200,
+  },
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    alignSelf: "flex-start",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
+  },
+  selected: {
+    backgroundColor: "coral",
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "coral",
+  },
+  selectedLabel: {
+    color: "white",
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
