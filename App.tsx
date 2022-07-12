ad319a835479e6a60af791d53041de5c1a16770c
yoshimura <karateyosi@gmail.com>
2022-07-12 17:39:18 +0900

    フォーム関連を試した

--- App.tsx
+++ App.tsx
@@ -1,122 +1,125 @@
+import React, { useState } from "react";
 import {
-  ScrollView,
+  Button,
   StyleSheet,
+  TouchableHighlight,
+  View,
   Text,
   TouchableOpacity,
-  View,
+  TouchableNativeFeedback,
+  Platform,
+  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
-import { ItemType, useFlexDirectionBasics } from "./hooks";
 
 export default function App() {
-  return <FlexDirectionBasics />;
-}
+  const onPressButton = () => {
+    alert("You tapped the button.");
+  };
+  const onLongPressButton = () => {
+    alert("You long-pressed the button!");
+  };
 
-const FlexDirectionBasics = () => {
-  const { items } = useFlexDirectionBasics();
+  const [text, setText] = useState("");
 
   return (
-    <ScrollView style={{ marginTop: 40, marginBottom: 20 }}>
-      {items.map((item) => (
-        <PreviewLayout
-          label={item.label}
-          values={item.values}
-          selectedValue={item.selectedValue}
-          setSelectedValue={item.setSelectedValue}
-        >
-          <View style={[styles.box, { backgroundColor: "powderblue" }]} />
-          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
-          <View
-            style={[
-              styles.box,
-              { backgroundColor: "steelblue" },
-              item.extendStyle,
-            ]}
-          />
-        </PreviewLayout>
-      ))}
-    </ScrollView>
-  );
-};
-
-type PreviewLayoutType = {
-  children: JSX.Element[];
-} & ItemType;
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}: PreviewLayoutType) => {
-  return (
-    <View style={{ padding: 10, flex: 1 }}>
-      <Text style={styles.label}>{label}</Text>
-      <View style={styles.row}>
-        {values.map((value) => (
-          <TouchableOpacity
-            key={value}
-            onPress={() => setSelectedValue(value)}
-            style={[styles.button, selectedValue === value && styles.selected]}
-          >
-            <Text
-              style={[
-                styles.buttonLabel,
-                selectedValue === value && styles.selectedLabel,
-              ]}
-            >
-              {value}
-            </Text>
-          </TouchableOpacity>
-        ))}
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button onPress={onPressButton} title="Press Me" />
       </View>
-      <View style={[styles.container, { [label]: selectedValue }]}>
-        {children}
+      <View style={styles.buttonContainer}>
+        <Button onPress={onPressButton} title="PressMe" color="#841584" />
+      </View>
+      <View style={styles.alternativeLayoutButtonContainer}>
+        <Button onPress={onPressButton} title="This looks great." />
+        <Button onPress={onPressButton} title="OK" color="#841584" />
+      </View>
+      <View style={styles.container}>
+        <TouchableHighlight onPress={onPressButton} underlayColor="white">
+          <View style={styles.touchableButton}>
+            <Text style={styles.touchableButtonText}>
+              TouchableButtonHighlight
+            </Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={onPressButton}>
+          <View style={styles.touchableButton}>
+            <Text style={styles.touchableButtonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback
+          onPress={onPressButton}
+          background={
+            Platform.OS === "android"
+              ? TouchableNativeFeedback.SelectableBackground()
+              : undefined
+          }
+        >
+          <View style={styles.touchableButton}>
+            <Text style={styles.touchableButtonText}>
+              TouchableWithoutFeedback&nbsp;
+              {Platform.OS !== "android" ? "(Android only)" : ""}
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback onPress={onPressButton}>
+          <View style={styles.touchableButton}>
+            <Text style={styles.touchableButtonText}>
+              TouchableWithoutFeedback
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight
+          onPress={onPressButton}
+          onLongPress={onLongPressButton}
+          underlayColor="white"
+        >
+          <View style={styles.touchableButton}>
+            <Text style={styles.touchableButtonText}>
+              Touchable with Long Press
+            </Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 40, borderColor: "red", borderWidth: 1 }}
+          placeholder="Type here to translate."
+          onChangeText={(newText) => setText(newText)}
+          defaultValue={text}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {text
+            .split(" ")
+            .map((word) => word && "🍎")
+            .join(" ")}
+        </Text>
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 8,
-    backgroundColor: "aliceblue",
-    height: 200,
+    justifyContent: "center",
   },
-  box: {
-    width: 50,
-    height: 50,
+  buttonContainer: {
+    margin: 20,
   },
-  row: {
+  alternativeLayoutButtonContainer: {
+    margin: 20,
     flexDirection: "row",
-    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: "oldlace",
-    alignSelf: "flex-start",
-    marginHorizontal: "1%",
-    marginBottom: 6,
-    minWidth: "48%",
+  touchableButton: {
+    marginBottom: 30,
+    alignItems: "center",
+    backgroundColor: "#6F3219",
+  },
+  touchableButtonText: {
     textAlign: "center",
-  },
-  selected: {
-    backgroundColor: "coral",
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: "500",
-    color: "coral",
-  },
-  selectedLabel: {
+    padding: 20,
     color: "white",
-  },
-  label: {
-    textAlign: "center",
-    marginBottom: 10,
-    fontSize: 24,
   },
 });
